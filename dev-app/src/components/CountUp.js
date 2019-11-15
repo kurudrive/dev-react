02cbb4cb4c673a49d69608697e208244d2e177b6
kurudrive <kurudrive@gmail.com>
2019-11-15 15:17:56 +0900

    コメント追加＆updateなど

--- dev-app/src/components/CountUp.js
+++ dev-app/src/components/CountUp.js
@@ -1,31 +1,36 @@
-import React from 'react';
+import React from "react";
 
-class CountUp extends React.Component{
-
-	// constructorは読み込まれたらとりあえず実行される部分
-	constructor(props) {
+class CountUp extends React.Component {
+  // constructorは読み込まれたらとりあえず実行される部分
+  constructor(props) {
     super(props);
     // stateを定義 オブジェクト形式で入れられる
-    this.state = {count:0}
+    this.state = { count: 0 };
   }
 
-	// State の countを変更するためのメソッドを定義
-	countUp(){
-    this.setState({count: this.state.count +1 });
+  // State の countを変更するためのメソッドを定義
+  countUp() {
+    this.setState({ count: this.state.count + 1 });
   }
 
-	render(){
-
-		// ここは普通のjsなのでコメント書けます（ return()の中だと{}が必要 ）
-		console.log(this.state.count);
-		//
+  render() {
+    // ここは普通のjsなのでコメント書けます（ return()の中だと{}が必要 ）
+    console.log(this.state.count);
+    //
     return (
-			<div className="text-center mb-5">
-			<h2>カウントアップ</h2>
-			<h3>{this.state.count}</h3>
-			 <button className="btn btn-primary" onClick={() => {this.countUp()}}>+</button>
-			</div>
-    )
+      <div className="text-center mb-5">
+        <h2>カウントアップ</h2>
+        <h3>{this.state.count}</h3>
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            this.countUp();
+          }}
+        >
+          +
+        </button>
+      </div>
+    );
   }
 }
 
