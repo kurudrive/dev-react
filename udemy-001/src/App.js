d74de58d9ae4b7af4f7d6ccdd2273eb2803ea45f
kurudrive <kurudrive@gmail.com>
2020-01-09 02:15:51 +0900

    関数コンポーネント

--- udemy-001/src/App.js
+++ udemy-001/src/App.js
@@ -1,20 +1,41 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class App extends Component{
-  render() {
-    // const greeting = "Hi";
-    // const dom = <div><h1 className="foo">{greeting}</h1></div>;
-    // return dom;
-    // return <input type="text" onClick={() => { console.log('aaa') }} />;
-    // return <input type="text" onChange={() => { console.log('aaa') }} />; 
-    return (
-      // React.Fragment で外側のタグを省略できる
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => { console.log('aaa') }} />
-      </React.Fragment>
-    )
-  }
+// class　コンポーネントだと Component を使ってないのでビルド時に warning が出るので読み込まない
+import React from 'react';
+
+// classコンポーネント
+// class App extends Component{
+//   render() {
+//     // const greeting = "Hi";
+//     // const dom = <div><h1 className="foo">{greeting}</h1></div>;
+//     // return dom;
+//     // return <input type="text" onClick={() => { console.log('aaa') }} />;
+//     // return <input type="text" onChange={() => { console.log('aaa') }} />; 
+//     return (
+//       // React.Fragment で外側のタグを省略できる
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => { console.log('aaa') }} />
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+// 関数コンポーネント
+const App = () => {
+  return (
+    <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+    </div>
+  )
 }
+
+const Cat = () => {
+  return <div>にゃー</div>
+}
+
 
 export default App;
