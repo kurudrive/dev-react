3cdc07fddd87ba8a5e3fc8872482cc0be21c1a06
kurudrive <kurudrive@gmail.com>
2020-01-08 19:18:23 +0900

    React.Fragment

--- udemy-001/src/App.js
+++ udemy-001/src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return <div>うえーい！</div>;
+class App extends Component{
+  render() {
+    // const greeting = "Hi";
+    // const dom = <div><h1 className="foo">{greeting}</h1></div>;
+    // return dom;
+    // return <input type="text" onClick={() => { console.log('aaa') }} />;
+    // return <input type="text" onChange={() => { console.log('aaa') }} />; 
+    return (
+      // React.Fragment で外側のタグを省略できる
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => { console.log('aaa') }} />
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
