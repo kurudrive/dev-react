fb0169ca545a23095209d409b8568d275e53a189
kurudrive <kurudrive@gmail.com>
2020-01-10 03:24:00 +0900

    16. prop-types 型チェック

--- udemy-001/src/App.js
+++ udemy-001/src/App.js
@@ -1,12 +1,13 @@
 // import React, { Component } from 'react';
 // class　コンポーネントだと Component を使ってないのでビルド時に warning が出るので読み込まない
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const App = () => {
   const profiles = [
     { name: "Taro", age: 10 },
     { name: "花子", age: 12 },
-    { name: "瑛太" },
+    { name: 2 },
   ]
   return (
     <div>
@@ -26,8 +27,15 @@ return <div>よう！ 俺は {props.name} だぜ！ {props.age}歳だよ！</div
 }
 
 // Props がない場合のデフォルト値を設定できる
-User.defaultProps = {
-  age:1
+// User.defaultProps = {
+//   age:1
+// }
+
+// 型チェック
+User.propTypes = {
+  name: PropTypes.string,
+  // .isRequired だと必須になる
+  age: PropTypes.number.isRequired
 }
 
 export default App;
